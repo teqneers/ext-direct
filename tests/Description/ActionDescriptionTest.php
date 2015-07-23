3651110332be220c5f6e8bb0daf8bd3feae65048
Stefan Gehrig <gehrig@teqneers.de>
2015-07-23 15:03:33 +0200

    began adding tests for service description

---
+++ tests/Description/ActionDescriptionTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefan
+ * Date: 23.07.15
+ * Time: 15:00
+ */
+
+namespace TQ\ExtDirect\Tests\Description;
+
+/**
+ * Class ActionDescriptionTest
+ *
+ * @package TQ\ExtDirect\Tests\Description
+ */
+class ActionDescriptionTest extends \PHPUnit_Framework_TestCase
+{
+}
